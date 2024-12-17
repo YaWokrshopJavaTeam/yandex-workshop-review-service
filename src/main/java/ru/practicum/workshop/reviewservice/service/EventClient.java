45df039f3e2b94cb78868a8c18904c78ce40bcdb
KurakinPavel <pu.kurakin@yandex.ru>
2024-12-17 16:32:37 +0300

    Review-service: add-openfeign (4) Review 1

--- src/main/java/ru/practicum/workshop/reviewservice/service/EventClient.java
+++ src/main/java/ru/practicum/workshop/reviewservice/service/EventClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import ru.practicum.workshop.reviewservice.dto.EventResponse;
 
-@FeignClient(name = "event-service-client", url = "http://host.docker.internal:8082")
+@FeignClient(name = "event-service-client", url = "http://localhost:8082")
 public interface EventClient {
     @GetMapping("/events/{id}")
     EventResponse readEventById(@RequestHeader("X-User-Id") Long userId, @PathVariable("id") Long id);
