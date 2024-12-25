5e67ff45e55bcfc5e8bff5363a7996932335cf66
KurakinPavel <pu.kurakin@yandex.ru>
2024-12-25 14:43:19 +0300

    Review Service add openfeign: Review 2

--- src/main/java/ru/practicum/workshop/reviewservice/client/RegistrationClient.java
+++ src/main/java/ru/practicum/workshop/reviewservice/client/RegistrationClient.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import ru.practicum.workshop.reviewservice.client.config.CustomFeignClientConfiguration;
 
-@FeignClient(name = "registration-service-client", url = "http://host.docker.internal:8084")
+@FeignClient(name = "registration-service-client", url = "http://host.docker.internal:8084",
+        configuration = CustomFeignClientConfiguration.class)
 public interface RegistrationClient {
     @GetMapping("/registrations/internal/status-of-registration/{eventId}")
     String getStatusOfRegistration(@PathVariable @Positive Long eventId,
